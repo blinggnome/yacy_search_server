13c8b6c732b6f9c8aedb7ce90bf8e13c1dfa522f
blinggnome <220555374+blinggnome@users.noreply.github.com>
2026-06-28 13:48:21 -0600

    Fix crawler rejection rule list rendering

--- source/net/yacy/htroot/CrawlerContentRejection_p.java
+++ source/net/yacy/htroot/CrawlerContentRejection_p.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
+import net.yacy.document.parser.html.CharacterCoding;
 import net.yacy.cora.protocol.RequestHeader;
 import net.yacy.crawler.data.CrawlerContentRejection;
 import net.yacy.data.ListManager;
@@ -56,13 +57,13 @@ public class CrawlerContentRejection_p {
         final StringBuilder rulesText = new StringBuilder();
         for (int i = 0; i < rules.size(); i++) {
             final String rule = rules.get(i);
-            prop.putNum("rules_" + i + "_count", i);
-            prop.putHTML("rules_" + i + "_rule", rule);
-            prop.put("rules_" + i + "_dark", (i & 1) == 0 ? "1" : "0");
+            prop.put("hasRules_ruleList_" + i + "_count", i);
+            prop.put("hasRules_ruleList_" + i + "_rule", CharacterCoding.unicode2html(rule, true));
+            prop.put("hasRules_ruleList_" + i + "_dark", (i & 1) == 0 ? "1" : "0");
             rulesText.append(rule).append('\n');
         }
-        prop.put("rules", rules.size());
-        prop.putHTML("rulesText", rulesText.toString());
+        prop.put("hasRules_ruleList", rules.size());
+        prop.put("rulesText", CharacterCoding.unicode2html(rulesText.toString(), true));
 
         return prop;
     }
